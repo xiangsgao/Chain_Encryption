8284c6fa60927ce9c68751673d74126917182d8c
GAO23 <gaoxiangshuai@gmail.com>
2017-10-14 18:37:57 -0400

    updated

--- Java_Version/Text_Crypt_GUI_Java/src/Application/logic/Form_Controller/mainSceneController.java
+++ Java_Version/Text_Crypt_GUI_Java/src/Application/logic/Form_Controller/mainSceneController.java
@@ -2,7 +2,6 @@ package Application.logic.Form_Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,7 +11,6 @@ public class mainSceneController implements Initializable{
 	@FXML 
 	private Button browseButton;
 	
-	@FXML Label filePathLabel;
 
 	
 	
