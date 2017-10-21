aadd838abc68b704721d7387a1058d2806d922dd
GAO23 <gaoxiangshuai@gmail.com>
2017-10-21 19:45:28 -0400

    Android version started

---
+++ Android_Version/Text_Crypt_Android/app/src/main/java/xgao/com/text_crypt_android/MainActivity.java
@@ -0,0 +1,13 @@
+package xgao.com.text_crypt_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Android_Version/Text_Crypt_Android/app/src/main/java/xgao/com/text_crypt_android/logic/model.java
@@ -0,0 +1,8 @@
+package xgao.com.text_crypt_android.logic;
+
+/**
+ * Created by xgao on 10/21/17.
+ */
+
+public class model {
+}
