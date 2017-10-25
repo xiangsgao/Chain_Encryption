2b0659dbcaaa137f54e6d5cf25e72b24bfc49688
GAO23 <gaoxiangshuai@gmail.com>
2017-10-25 15:28:37 -0400

    updated

--- Android_Version/Text_Crypt_Android/app/src/main/java/xgao/com/text_crypt_android/MainActivity.java
+++ Android_Version/Text_Crypt_Android/app/src/main/java/xgao/com/text_crypt_android/MainActivity.java
@@ -2,12 +2,18 @@ package xgao.com.text_crypt_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import xgao.com.text_crypt_android.logic.model;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private model model;
+    private Button convert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        model = new model();
     }
 }
