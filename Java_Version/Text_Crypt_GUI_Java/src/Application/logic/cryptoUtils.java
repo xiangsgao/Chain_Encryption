b68370c9409adbeb7c67201d794ea4c0b59803fc
GAO23 <gaoxiangshuai@gmail.com>
2017-10-28 16:48:16 -0400

    fixed windows and generated jar

--- Java_Version/Text_Crypt_GUI_Java/src/Application/logic/cryptoUtils.java
+++ Java_Version/Text_Crypt_GUI_Java/src/Application/logic/cryptoUtils.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
@@ -26,6 +25,7 @@ import javax.crypto.spec.SecretKeySpec;
  * All the original codes belong to the original author
  */
 public class cryptoUtils {
+	//private static final String ALGORITHM = "AES";
     private static final String TRANSFORMATION = "AES";
  
     public static void encrypt(String key, File inputFile, File outputFile)
@@ -38,14 +38,14 @@ public class cryptoUtils {
         doCrypto(Cipher.DECRYPT_MODE,key, inputFile, outputFile);
     }
  
-    private static void doCrypto(int cipherMode, String key, File inputFile,
-            File outputFile) throws cryptoException {
+    private static void doCrypto(int cipherMode, String key, File inputFile, File outputFile) throws cryptoException {
     	// This generates the key, helpful if user enters key not in 16, 32, 64, or 128 bytes
-    	Key crytoKey = generateCrytoKey(key);
+    	SecretKey crytoKey = generateCrytoKey(key);
         try {
         	if(key == null) {
         		throw new cryptoException("Key conversion error");
         	}
+        	//Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(cipherMode, crytoKey);
             FileInputStream inputStream = new FileInputStream(inputFile);
@@ -63,7 +63,7 @@ public class cryptoUtils {
         } catch (NoSuchPaddingException | NoSuchAlgorithmException
                 | InvalidKeyException | BadPaddingException
                 | IllegalBlockSizeException | IOException ex) {
-            throw new cryptoException("Error encrypting/decrypting file\nTry checking if the file you selected is encrypted or not\nAlso check if the key is correct when decrypting", ex);
+            throw new cryptoException("Error encrypting/decrypting file\nTry checking if the file you selected is encrypted or not\nAlso check if the key is correct when decrypting\n" + ex.toString(), ex);
         }
     }
     
@@ -75,7 +75,7 @@ public class cryptoUtils {
 		SecretKeyFactory factory;
 		try {
 			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-			KeySpec spec = new PBEKeySpec(charKey, salt, 65536, 256);
+			KeySpec spec = new PBEKeySpec(charKey, salt, 65536, 128);
 			SecretKey tmp = factory.generateSecret(spec);
 			SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
 			return secret;
